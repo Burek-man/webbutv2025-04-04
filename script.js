a25f27de6692904ffc26e2d3e20b614a1e9407a9
Noah <Alnu07001@utb.vaxjo.se>
2025-04-25 11:06:57 +0200

    aa

---
+++ script.js
@@ -0,0 +1,4 @@
+function LeDunk(){
+    document.getElementById('Ledunk').style.display = "block"
+    document.getElementById('LeAudio').muted = document.getElementById('LeAudio').muted ? false : true;
+}
